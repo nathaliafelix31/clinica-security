cc6283bc26efab4ea87ee2a21682b12b5890563b
nathaliafelix31 <nathy.felix.31@gmail.com>
2024-06-02 17:57:33 -0300

    gerenciando sessões simultâneas

--- src/main/java/com/felix/clinicaSecurity/web/controller/HomeController.java
+++ src/main/java/com/felix/clinicaSecurity/web/controller/HomeController.java
@@ -1,6 +1,9 @@
 package com.felix.clinicaSecurity.web.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.security.web.authentication.session.SessionAuthenticationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,12 +26,33 @@ public class HomeController {
 
 	// login invalido
 	@GetMapping("/login-error")
-	public String loginError(ModelMap model) {
+	public String loginError(ModelMap model, HttpServletRequest resp) {
 
+		HttpSession session = resp.getSession();
+		String lastException = String.valueOf(session.getAttribute("SPRING_SECURITY_LAST_EXCEPTION"));
+		if (lastException.contains(SessionAuthenticationException.class.getName())){
+			model.addAttribute("alerta", "erro");
+			model.addAttribute("titulo", "Acesso Recusado!");
+			model.addAttribute("texto", "Você já está logado em outro dispositivo.");
+			model.addAttribute("subtexto", "Faça o logout ou espere sua sessão expirar.");
+
+			return "login";
+		}
 		model.addAttribute("alerta", "erro");
 		model.addAttribute("titulo", "Credenciais Inválidas!");
 		model.addAttribute("texto", "Login ou senha incorretos, tente novamente.");
 		model.addAttribute("subtexto", "Acesso permitido apenas para cadastros já ativados.");
+
+		return "login";
+	}
+
+	@GetMapping("/expired")
+	public String sessaoExpirada(ModelMap model) {
+
+		model.addAttribute("alerta", "erro");
+		model.addAttribute("titulo", "Acesso recusado!");
+		model.addAttribute("texto", "Sua sessão expirou.");
+		model.addAttribute("subtexto", "Você logou em outro dispositivo.");
 
 		return "login";
 	}
